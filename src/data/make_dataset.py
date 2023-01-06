83c5a104c536f8d2b4c460f232849b5f4dea74ad
unknown <enache.denisa98@gmail.com>
2023-01-06 18:02:01 +0100

    Updated make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,17 +3,51 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+import os
+import zipfile
+import pandas
+import torch
+import numpy as np
 
 
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+@click.argument('dataset_path', type=click.Path())
+def main(dataset_path):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    logger.info('Downloading dataset from kaggle')
+    path = os.path.join(dataset_path, "raw")
+
+    try:
+        import kaggle
+    except:
+        logger.warning("kaggle no imported")
+    
+    try:
+        kaggle.api.competition_download_files("sms-spam-collection-dataset", path=path)
+    except Exception:
+        logger.warning("Downloaded failed")
+
+    out_folder_interim = os.path.join(dataset_path, "interim")
+    os.makedirs(out_folder_interim, exist_ok=True)
+    with zipfile.ZipFile(os.path.join(path, "sms_spam.zip"), 'r') as zip_ref:
+        zip_ref.extractall(out_folder_interim)
+    
+    path_csv = os.path.join(out_folder_interim, "spam.csv")
+    dataset = pandas.read_csv(path_csv, encoding='latin-1')
+
+    # Dropping unwanted columns
+    dataset.drop(["Unnamed: 2", "Unnamed: 3", "Unnamed: 4"], inplace=True, axis=1)
+    # Chaning the labels for convinience
+    dataset["v1"].replace({"ham": 0, "spam":1}, inplace=True)
+    # Changing the column names for better 
+    dataset.rename({"v1": "message_type", "v2": "original_message"},axis=1, inplace=True)
+   
+    message_type = dataset[dataset.columns[0]]
+    orig_message = dataset[dataset.columns[1]]
+    logger.info(orig_message)
 
 
 if __name__ == '__main__':
